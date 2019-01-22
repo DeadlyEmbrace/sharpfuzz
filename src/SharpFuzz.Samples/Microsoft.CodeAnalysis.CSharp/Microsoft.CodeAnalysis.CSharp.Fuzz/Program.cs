d534ac650b3e20df1a920a456f826669a00d57d4
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-01-22 10:33:48 +0100

    Add Roslyn

---
+++ src/SharpFuzz.Samples/Microsoft.CodeAnalysis.CSharp/Microsoft.CodeAnalysis.CSharp.Fuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SharpFuzz;
+
+namespace Microsoft.CodeAnalysis.CSharp.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.Run(() =>
+			{
+				var text = File.ReadAllText(args[0]);
+				var tree = CSharpSyntaxTree.ParseText(text);
+				var root = (CompilationUnitSyntax)tree.GetRoot();
+			});
+		}
+	}
+}
---
+++ src/SharpFuzz.Samples/Microsoft.CodeAnalysis.CSharp/Microsoft.CodeAnalysis.CSharp.Run/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.CodeAnalysis.CSharp.Run
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = File.ReadAllText(args[0]);
+			var tree = CSharpSyntaxTree.ParseText(text);
+			var root = (CompilationUnitSyntax)tree.GetRoot();
+		}
+	}
+}
